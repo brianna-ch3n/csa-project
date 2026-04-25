5385817a8baa76ac018b098c821f1c5cd6641730
brianna-ch3n <brianna666888@gmail.com>
2026-04-24 20:37:59 -0700

    Refactor DataManager for new CSV and analysis
    Updated the file name to 'IMDB Top 200.csv' and modified the data parsing logic to handle up to 8 fields. Added calculations for mean rating, best and worst movies, and count of movies rated 8.5 or higher.

--- DataManager.java
+++ DataManager.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class DataManager {
     public static void main(String[] args) {
         ArrayList<IMDB> roster = new ArrayList<>();
-        String fileName = "IMDB Top 1000.csv";
+        String fileName = "IMDB Top 200.csv";
 
         try {
             Scanner fileReader = new Scanner(new File(fileName));
@@ -19,10 +19,22 @@ public class DataManager {
                 String line = fileReader.nextLine();
                 
                 // 3. Split the line by comma
-                String[] data = line.split(",");
-                
-                // 4. Parse data and create movie object
-                // TODO: roster.add(new Athlete(...));
+                String[] data = line.split(",", 8);
+
+                if (data.length >= 8) {
+
+                    String movie = data[0];
+                    int year = (int) Double.parseDouble(data[1]);
+                    int duration = Integer.parseInt(data[2].replace(" min", ""));
+                    String genre = data[3];
+                    double rating = Double.parseDouble(data[4]);
+                    String star1 = data[5];
+                    String star2 = data[6];
+                    String star3 = data[7];
+
+                    IMDB film = new IMDB(movie, year, duration, genre, rating, star1, star2, star3);
+                    roster.add(film);
+                }
             }
             
             fileReader.close();
@@ -33,7 +45,58 @@ public class DataManager {
         } catch (NumberFormatException e) {
             System.out.println("Error: Failed to parse a numeric value in the CSV.");
         }
+        
+        if(roster.size()>0){
+            System.out.println(roster.get(0));
+        }
+        System.out.println("Total movies: " + roster.size());
 
+        
         // 5. TODO: Implement your Level Up analysis here!
+        //mean
+        double sum = 0;
+
+        for (IMDB m:roster){
+            sum+= m.getImdbRating();
+        }
+
+        double mean = sum/roster.size();
+
+        //best movie
+        IMDB best = roster.get(0);
+
+        for (IMDB m: roster){
+            if (m.getImdbRating()> best.getImdbRating()){
+                best = m;
+            }
+        }
+
+        //worst movie
+        IMDB worst = roster.get(0);
+
+        for(IMDB m:roster){
+            if(m.getImdbRating() < worst.getImdbRating()){
+                worst = m;
+            }
+        }
+
+        //
+        int count = 0;
+
+        for (IMDB m:roster){
+            if(m.getImdbRating() >= 8.5){
+                count++;
+            }
+        } 
+
+        // final output
+        System.out.println("\nIMDB ANALYSIS");
+        System.out.println("Total Movies: " + roster.size());
+        System.out.println("Average Rating: " + mean);
+        System.out.println("Best Movie: " + best);
+        System.out.println("Worst Movie: " + worst);
+        System.out.println("Movies rated 8.5+: " + count);
+    
+        
     }
 }
